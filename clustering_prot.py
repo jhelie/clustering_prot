3c1632f7ba158e59cc7f1e871b3dd19caef00f51
jhelie <jean.helie@gmail.com>
2015-05-26 16:48:06 +0100

    v0.0.5

--- clustering_prot.py
+++ clustering_prot.py
@@ -21,8 +21,6 @@ author: Jean Helie (jean.helie@bioch.ox.ac.uk)
 contributors: Anna Duncan, Matthieu Chavent
 git: https://github.com/jhelie/clustering_prot
 **********************************************
-
-#test#
 	
 [ DESCRITPION ]
 
