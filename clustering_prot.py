05be864123304877f0edf0c099b4a43e91f6a36f
jhelie <jean.helie@gmail.com>
2015-10-13 10:14:26 +0100

    v0.0.9

--- clustering_prot.py
+++ clustering_prot.py
@@ -22,7 +22,7 @@ contributors: Anna Duncan, Matthieu Chavent
 git: https://github.com/jhelie/clustering_prot
 **********************************************
 	
-[ DESCRITPION ]
+[ DESCRITPIONS ]
 
 This script calculates the evolution of proteins clustering status.
 	
